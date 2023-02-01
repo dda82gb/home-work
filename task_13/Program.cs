d075e6b10c250c78c526f649f28f3f4ba09de842
Davran_Djumanov <dda_82@mail.ru>
2023-02-02 02:00:22 +0600

    add task_13

---
+++ task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+// третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число :");
+int num = int.Parse(Console.ReadLine());
+if ( num <=99 )
+{
+ Console.BackgroundColor = ConsoleColor.Red;
+ Console.WriteLine("->Tретьей цифры нет");
+}
+else
+{
+ Console.ForegroundColor = ConsoleColor.Green;
+ string Num = Convert.ToString(num); 
+ Console.WriteLine("Третья цифра этого числа-->"+Num[2]);
+ Console.BackgroundColor = ConsoleColor.Blue;
+ Console.WriteLine("Ураа! это точно "+Num[2]);
+}
