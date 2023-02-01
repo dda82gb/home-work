e10a96b118fbf7d7fc5e0dbee5a377f0dd34db15
Davran_Djumanov <dda_82@mail.ru>
2023-02-02 00:05:58 +0600

    add task_10

---
+++ task_10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+
+Console.Clear();
+Console.Write("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+if ( num <=99 || 1000<=num)
+{
+ Console.ForegroundColor = ConsoleColor.Red;
+ Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+ Console.ForegroundColor = ConsoleColor.Green;
+ string Num = Convert.ToString(num);   
+ Console.WriteLine("вторая цифра этого числа -> "+ Num[1]);
+}
